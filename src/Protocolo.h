6b753c4151c690efa36081a88640437098d9b0ec
tiagosilveira1 <141841534+tiagosilveira1@users.noreply.github.com>
2026-09-19 19:21:07 -0300

    Refactor Protocolo.h with new game settings
    Updated protocol definitions and added new constants for game configuration.

--- src/Protocolo.h
+++ src/Protocolo.h
@@ -1,26 +1,40 @@
 #ifndef PROTOCOLO_H
 #define PROTOCOLO_H
 
+/*
+ * Configurações do servidor.
+ */
 #define PORTA 7070
-#define MAX_JOGADORES 2
+#define MAX_CLIENTES 2
+
+/*
+ * Configurações do jogo.
+ */
 #define NUM_RODADAS 5
 #define TEMPO_RODADA 10
+#define MIN_CARACTERES 5
 
+/*
+ * Tamanhos dos dados.
+ */
+#define TAM_NOME 50
+#define TAM_PALAVRA 100
 #define TAM_MENSAGEM 256
 
-#define MSG_MSG           "MSG"
-#define MSG_AGUARDE       "AGUARDE"
-#define MSG_NOME          "NOME"
-#define MSG_RODADA        "RODADA"
-#define MSG_RESULTADO     "RESULT"
-#define MSG_PLACAR        "PLACAR"
-#define MSG_FIM           "FIM"
-#define MSG_SAIR          "QUIT"
-
-#define MSG_CS_NOME       "NOME"
-#define MSG_CS_PALAVRA    "PALAVRA"
-#define MSG_CS_TIMEOUT    "TIMEOUT"
-
+/*
+ * Mensagens do protocolo.
+ *
+ */
+#define MSG "MSG"
+#define NOME "NOME"
+#define AGUARDE "AGUARDE"
+#define RODADA "RODADA"
+#define PALAVRA "PALAVRA"
+#define TIMEOUT "TIMEOUT"
+#define RESULTADO "RESULTADO"
+#define PLACAR "PLACAR"
+#define FIM "FIM"
+#define SERVIDOR_CHEIO "FULL"
 #define SEPARADOR "|"
 
 #endif
